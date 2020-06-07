52e6a8cc430634e335c960eaf141ad5ad04abe65
kuldeep <kdit13@gmail.com>
2020-06-07 17:55:19 +0530

    first python commit

---
+++ test.py
@@ -0,0 +1,18 @@
+from sys import argv
+#print(type(argv))
+##----------------------------------------------------------
+##print("The Number of Command Line Arguments: ",len(argv))
+##print("The List of Command Line Arguments : ",argv)
+##print("Command Line Arguments one by one: ")
+##for x in argv:
+##    print(x)
+##----------------------------------------------------------
+name = 'Durga'
+salary = 10000
+gf = 'Sunny'
+##print("Hello {0} your salary is {1} and your friend {2} is waiting".format(name,salary,gf))
+##print("Hello {x} your salary is {y} and your friend {z} is waiting".format(x=name,y=salary,z=gf))
+##for x in range(1,21,2):
+##    print(x)
+for x in range(10,0,-1):
+    print(x)
